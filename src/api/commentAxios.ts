a813a019cd4e096cee65f2927b5eb9306b4c663d
Lee jin <83453646+j-nary@users.noreply.github.com>
2024-08-01 06:36:22 +0900

    feat: comment API 작성

---
+++ src/api/commentAxios.ts
@@ -0,0 +1,36 @@
+import { authAxios } from "@/api"
+
+export const getCommentAxios = (solutionId: number) => {
+  return authAxios.get(
+    '/comment',
+    {
+      params: { solutionId }
+    }
+  )
+};
+
+export interface CommentAPIPropType {
+  solutionId: number;
+  content: string;
+}
+
+export const postCommentAxios = ({ solutionId, content }: CommentAPIPropType) => {
+  return authAxios.post(
+    '/comment',
+    {
+      body: {
+        solutionId,
+        content
+      }
+    }
+  )
+};
+
+export const deleteCommentAxios = (commentId: number) => {
+  return authAxios.delete(
+    '/comment',
+    {
+      params: { commentId }
+    }
+  )
+};
---
+++ src/hooks/query/useCommentQuery.ts
@@ -0,0 +1,31 @@
+import { CommentAPIPropType, deleteCommentAxios, getCommentAxios, postCommentAxios } from "@/api/commentAxios"
+import { useMutation, useQuery } from "@tanstack/react-query"
+
+export const useGetComment = (solutionId: number) => {
+  return useQuery({
+    queryKey: ['getComment', solutionId],
+    queryFn: () => getCommentAxios(solutionId).then(res => res.data),
+  })
+};
+
+export const usePostComment = () => {
+  const mutation = useMutation({
+    mutationFn: ({ solutionId, content }: CommentAPIPropType) => postCommentAxios({ solutionId, content }),
+    onError: (error) => {
+      console.log('post comment fetch error: ', error);
+    }
+  });
+
+  return mutation;
+}
+
+export const useDeleteComment = () => {
+  const mutation = useMutation({
+    mutationFn: (commentId: number) => deleteCommentAxios(commentId),
+    onError: (error) => {
+      console.log('delete comment fetch error: ', error);
+    }
+  });
+
+  return mutation;
+}
